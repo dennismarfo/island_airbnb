c8ad2e9875df8193afde42e7d3f504488218d48f
Théau Bruno <bn.theau@gmail.com>
2019-08-13 14:56:49 +0200

    ajout controller user_id property_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_093641) do
+ActiveRecord::Schema.define(version: 2019_08_13_125533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "check_in_date"
+    t.string "check_out_date"
+    t.string "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_id"
+    t.string "property_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "name"

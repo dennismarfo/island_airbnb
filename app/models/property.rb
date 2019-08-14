42443a9c48536883e0e68f400ffff04b620efe8e
Denis Kojo <dennismarfo@gmail.com>
2019-08-14 12:07:23 +0200

    installation of geocoder gem

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,6 @@
 class Property < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
-
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
   validates :country, presence: true
@@ -9,4 +8,7 @@ class Property < ApplicationRecord
   validates :price, presence: true
   validates :category, presence: true, inclusion: { in: ["Island", "Beach"] }
   validates :photo, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end

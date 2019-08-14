aed55ba0dcfe61fdebb4c4b9669b5792a19d537e
Vincent Grenier <vincentgrenier@hotmail.fr>
2019-08-14 16:16:54 +0200

    gogogog

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
+
 const typedHome = () => {
+
   const options = {
   strings: ["Welcome to <i>OUR APP</i>.", "We glad to see you here. :) "],
   typeSpeed: 80
-};
+  };
 
-const typed = new Typed(".element", options);
-}
+  const typed = new Typed(".element", options);
+  }
 
 export { typedHome };

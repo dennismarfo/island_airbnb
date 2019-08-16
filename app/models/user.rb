72df795de563cc94c930c1625360a9bdf30d528b
Vincent Grenier <vincentgrenier@hotmail.fr>
2019-08-16 15:28:46 +0200

    go

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def confirmation_required?
+    !confirmed?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_220453) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
